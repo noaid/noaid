eb59e43b87c1551c518ea2039fa0517c3f2cb59b
ximing <807754634@qq.com>
2018-08-01 22:57:47 +0800

    feat: add env config

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@
  * Created by ximing on 2018/8/1.
  */
 'use strict';
+require('dotenv').config();
 const http = require('http');
 const koa = require('koa');
 const koaJson = require('koa-json');
 const bodyParser = require('koa-bodyparser');
 const koaRouter = require('./router');
 
-const port = 8080;
+const port = process.env.PORT;
 
 const app = new koa();
 app.use(bodyParser());
@@ -18,4 +19,5 @@ koaRouter(app);
 const server = http.createServer(app.callback());
 
 server.listen(port, () => {
+    console.log(`listen port is ${port}`);
 });
